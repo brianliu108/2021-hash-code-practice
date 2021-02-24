293173dace874dc9477f3c6614334f6eba0cf89e
JakeTS <timpbh@gmail.com>
2021-02-24 12:08:48 -0500

    ahh

--- 2020hashcodepractice/PizzaManager.cs
+++ 2020hashcodepractice/PizzaManager.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
-namespace _2020hashcodepractice
-{
+namespace _2020hashcodepractice {
     public class PizzaManager {
-        public readonly int numOfPizzas, team2Count, team3Count, team4Count;
+        public readonly int numOfPizzas;
+        public readonly int[] teamMax = new int[3];
         public List<Team>[] teams = new List<Team>[3];
         public int pizzasLeft { get; private set; }
 
@@ -11,26 +12,23 @@ namespace _2020hashcodepractice
 
         public PizzaManager(int numOfPizzas, int team2Count, int team3Count, int team4Count) {
             this.numOfPizzas = pizzasLeft = numOfPizzas;
-            this.team2Count = team2Count;
-            this.team3Count = team3Count;
-            this.team4Count = team4Count;   
-            
-            
+            teamMax[SIZE_TWO] = team2Count;
+            teamMax[SIZE_THREE] = team3Count;
+            teamMax[SIZE_FOUR] = team4Count;
         }
 
         public void addPizza(Pizza pizza) => pizzas.Add(pizza);
 
-        const int TWO = (int)Team.TeamSize.TWO;
-        const int THREE = (int)Team.TeamSize.THREE;
-        const int FOUR = (int)Team.TeamSize.FOUR;
+        const int SIZE_TWO = (int) Team.TeamSize.TWO;
+        const int SIZE_THREE = (int) Team.TeamSize.THREE;
+        const int SIZE_FOUR = (int) Team.TeamSize.FOUR;
 
         /// <summary>
         /// Solution 1
         /// </summary>
         /// <param name="team"></param>
-        public void givesPizzas_LargerTeams(Team team)
-        {            
-            pizzas[TWO].
+        public void givesPizzas_LargerTeams(Team team) {
+            pizzas[SIZE_TWO].
         }
 
         //public bool assignPizza(Pizza pizza, int teamMembers)

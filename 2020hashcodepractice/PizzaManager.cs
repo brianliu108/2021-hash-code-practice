38beb93b1a8305eeab073f2efc88c14431fc5421
JakeTS <timpbh@gmail.com>
2021-02-24 16:13:46 -0500

    pizzas left

--- 2020hashcodepractice/PizzaManager.cs
+++ 2020hashcodepractice/PizzaManager.cs
@@ -101,6 +101,7 @@ namespace _2020hashcodepractice {
             int minAcceptedDupes = 0;
 
             while (pizzasLeft > 0) {
+                Console.WriteLine("Pizzas left: " + pizzasLeft);
                 int minDupes = int.MaxValue;
                 for (int currentSize = SIZE_FOUR; currentSize >= SIZE_TWO; currentSize--) {
                     foreach (Team team in teams[currentSize]) {

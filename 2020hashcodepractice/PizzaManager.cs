5429a36cb842002bd9afeb0384456dc92c653965
JakeTS <timpbh@gmail.com>
2021-02-24 15:56:13 -0500

    fixed numbers

--- 2020hashcodepractice/PizzaManager.cs
+++ 2020hashcodepractice/PizzaManager.cs
@@ -65,8 +65,8 @@ namespace _2020hashcodepractice {
             StringBuilder s = new StringBuilder();
             s.Append("Team counts: " + (allValid ? "Good!" : "NOPE! NOPE! NOPE!")).Append("\n")
                 .Append($"  {(t2valid ? "✓ " : "")}Size 2 teams: {team2count}/{teamMax[SIZE_TWO]}\n")
-                .Append($"  {(t3valid ? "✓ " : "")}Size 3 teams: {team2count}/{teamMax[SIZE_THREE]}\n")
-                .Append($"  {(t4valid ? "✓ " : "")}Size 4 teams: {team2count}/{teamMax[SIZE_FOUR]}\n");
+                .Append($"  {(t3valid ? "✓ " : "")}Size 3 teams: {team3count}/{teamMax[SIZE_THREE]}\n")
+                .Append($"  {(t4valid ? "✓ " : "")}Size 4 teams: {team4count}/{teamMax[SIZE_FOUR]}\n");
             return s.ToString();
         }
 
